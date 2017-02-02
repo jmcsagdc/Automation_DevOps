4e9b9afc3ab15f9a22a72f0fdb3b6e3d15694c09
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-02 09:28:53 -0800

    Update nfsclientsidecreate.sh
    no more hardcoded uidNumber

--- nfsclientsidecreate.sh
+++ nfsclientsidecreate.sh
@@ -1,17 +1,41 @@
-# Run this on NFS SERVER and scp the output file to the clients for now
+newUsername=raw_input('Enter USERNAME: ')
 
+import os
+tempPass="1"+newUsername+"9"
+print("The user's temporary password is: "+tempPass+" so they need to change it!")
 
-# The following commented lines are there if we want to distinguish the 
-# mount locations from the OS versions of the same.
-#mkdir -p /mnt/nfs/home
-#mkdir -p /mnt/nfs/var/nfs
+bigstring="slappasswd -s "+tempPass
+myHash=os.popen(bigstring).read()
 
-#mount <server>:/home /mnt/nfs/home
-#mount <sever>:/var/nfs /mnt/nfs/var/nfs
+# Get unique uidNumber
+currentNumberOS="ldapsearch -x -b dc=jmcsagdc,dc=local | grep uidNumber | awk '{print $2}' | uniq | sort -g -r | head -1"
+currentNumber=os.popen(currentNumberOS).read()
+# Add one to it for next user
+nextUserNumber=int(currentNumber)+1
+nextUserNumber=str(nextUserNumber)
 
-echo "mount `hostname`:/home /home" >> nfsclients_mount.sh
-# echo "mount `hostname`:/var/nfs /var/nfs" >> nfsclients_mount.sh
-echo >> nfsclients_mount.sh
-echo "echo \"# Append to /etc/fstab\"" >> nfsclients_mount.sh
-echo "echo \"`hostname`:/home   /home   nfs    auto  0  0\"" >> nfsclients_mount.sh
-# echo "echo \"`hostname`:/var/nfs   /var/nfs   nfs    auto  0  0\"" >> nfsclients_mount.sh
+outfileName=newUsername+'.ldif'
+outfile=open(outfileName, "w")
+
+outfile.write('dn: uid='+newUsername+',ou=People,dc=jmcsagdc,dc=local\n')
+outfile.write('''objectClass: top
+objectClass: account
+objectClass: posixAccount
+objectClass: shadowAccount\n''')
+outfile.write('cn: '+newUsername+'\n')
+outfile.write('uid: '+newUsername+'\n')
+outfile.write('uidNumber: '+nextUserNumber+'\n')
+outfile.write('gidNumber: 100\n')
+outfile.write('homeDirectory: /home/'+newUsername+'\n')
+outfile.write('loginShell: /bin/bash\n')
+outfile.write('gecos: '+newUsername+' ['+newUsername+' (at) jmcsagdc]\n')
+outfile.write('userPassword: '+myHash)
+outfile.write('''shadowLastChange: 17058
+shadowMin: 0
+shadowMax: 99999
+shadowWarning: 7''')
+outfile.close()
+print('Adding to LDAP db. The next request is for the LDAP admin password.')
+ldifAddToDB='ldapadd -x -W -D "cn=ldapadm,dc=jmcsagdc,dc=local" -f '+outfileName
+dbAddResults=os.popen(ldifAddToDB).read()
+print dbAddResults+'\n'
