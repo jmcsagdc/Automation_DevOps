654ddde3c75ceca2dd3d9dab94b50ced59cd56a7
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-02 08:53:31 -0800

    Update user_ldif_creator.py
    added the logic for putting the new user into the LDAP db

--- user_ldif_creator.py
+++ user_ldif_creator.py
@@ -6,20 +6,20 @@ print("The user's temporary password is: "+tempPass+" so they need to change it!
 
 bigstring="slappasswd -s "+tempPass
 myHash=os.popen(bigstring).read()
-
-outfile=open(newUsername+'.ldif', "w")
+outfileName=newUsername+'.ldif'
+outfile=open(outfileName, "w")
 
 outfile.write('dn: uid='+newUsername+',ou=People,dc=jmcsagdc,dc=local\n')
 outfile.write('''objectClass: top
 objectClass: account
 objectClass: posixAccount
-objectClass: shadowAccount''')
+objectClass: shadowAccount\n''')
 outfile.write('cn: '+newUsername+'\n')
 outfile.write('uid: '+newUsername+'\n')
 outfile.write('''uidNumber: 10000
-gidNumber: 100''')
+gidNumber: 100\n''')
 outfile.write('homeDirectory: /home/'+newUsername+'\n')
-outfile.write('loginShell: /bin/bash')
+outfile.write('loginShell: /bin/bash\n')
 outfile.write('gecos: '+newUsername+' ['+newUsername+' (at) jmcsagdc]\n')
 outfile.write('userPassword: '+myHash)
 outfile.write('''shadowLastChange: 17058
@@ -27,5 +27,8 @@ shadowMin: 0
 shadowMax: 99999
 shadowWarning: 7''')
 outfile.close()
-
+print('Adding to LDAP db. The next request is for the LDAP admin password.')
+ldifAddToDB='ldapadd -x -W -D "cn=ldapadm,dc=jmcsagdc,dc=local" -f '+outfileName
+dbAddResults=os.popen(ldifAddToDB).read()
+print dbAddResults+'\n'
 #TODO Change the UID Number to something smarter
