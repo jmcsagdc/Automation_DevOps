64503ac1baa7e5951319339a068003217e1bd362
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-07 16:04:12 -0800

    Update add_one_nfs_client.sh
    made 3 mounts: /config /dev/ and /home
    
    The /dev one is a little odd...

--- add_one_nfs_client.sh
+++ add_one_nfs_client.sh
@@ -1,8 +1,19 @@
 # Note that there is no error checking.
 # This takes one IP in 'single quotes' as an argument
 
+if [ ! -d "/config" ]; then
+  mkdir /config
+fi
+
 IP=$1
-echo "/home             $IP(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/var/nfs  $IP(rw,sync,no_subtree_check)" >> /etc/exports
+echo "/home		$IP(rw,sync,no_root_squash,no_subtree_check)"	>> /etc/exports
+echo "echo \"/home		$IP(rw,sync,no_root_squash,no_subtree_check)\"	>> /etc/exports"
+echo "/dev		$IP(rw,sync,no_subtree_check)"					>> /etc/exports
+echo "echo \"/dev		$IP(rw,sync,no_subtree_check)\"					>> /etc/exports"
+echo "/config	$IP(rw,sync,no_subtree_check)"					>> /etc/exports
+echo "echo \"/config	$IP(rw,sync,no_subtree_check)\"					>> /etc/exports"
+
 exportfs -a
+echo "exportfs -a"
+echo "FINISHED!"
 showmount -e `hostname`
