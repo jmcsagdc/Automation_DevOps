2b56d6d0b6923ad0816b001e51472606bfe1a8a2
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-07 16:17:46 -0800

    Update add_one_nfs_client.sh
    moved directory create to nfs-server-create script

--- add_one_nfs_client.sh
+++ add_one_nfs_client.sh
@@ -1,10 +1,6 @@
 # Note that there is no error checking.
 # This takes one IP in 'single quotes' as an argument
 
-if [ ! -d "/config" ]; then
-  mkdir /config
-fi
-
 IP=$1
 echo "/home		$IP(rw,sync,no_root_squash,no_subtree_check)"	>> /etc/exports
 echo "echo \"/home		$IP(rw,sync,no_root_squash,no_subtree_check)\"	>> /etc/exports"
