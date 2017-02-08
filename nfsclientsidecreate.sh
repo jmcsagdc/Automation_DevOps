9259c86673d6a0260af75d64e6391c8bad118df6
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-08 10:19:13 -0800

    Update nfsclientsidecreate.sh
    fixed mount points and automated fstab alterations

--- nfsclientsidecreate.sh
+++ nfsclientsidecreate.sh
@@ -1,20 +1,20 @@
 # Run this on NFS SERVER and scp the output file to the clients for now
 
 
-# The following commented lines are there if we want to distinguish the 
+# The following commented lines are there so we distinguish the 
 # mount locations from the OS versions of the same.
-#mkdir -p /mnt/nfs/home
-#mkdir -p /mnt/nfs/var/nfs
-
-#mount <server>:/home /mnt/nfs/home
-#mount <sever>:/var/nfs /mnt/nfs/var/nfs
-
-echo "mount `hostname`:/home /home" >> nfsclients_mount.sh
-echo "mount `hostname`:/var/dev /dev" >> nfsclients_mount.sh
-echo "mount `hostname`:/var/config /config" >> nfsclients_mount.sh
+echo "echo \"#####################    Creating /var/nfs mount points   #####################\"" >> nfsclients_mount.sh
+echo "mkdir -p /var/nfs/home" >> nfsclients_mount.sh
+echo "mkdir -p /var/nfs/dev" >> nfsclients_mount.sh
+echo "mkdir -p /var/nfs/config" >> nfsclients_mount.sh
+echo "echo \"#####################    Mounting /var/nfs mount points   #####################\"" >> nfsclients_mount.sh
+echo "mount `hostname`:/home /var/nfs/home" >> nfsclients_mount.sh
+echo "mount `hostname`:/var/dev /var/nfs/dev" >> nfsclients_mount.sh
+echo "mount `hostname`:/var/config /var/nfs/config" >> nfsclients_mount.sh
 echo >> nfsclients_mount.sh
-echo "echo \"#####################    Echo into fstab on CLIENT   #####################\"" >> nfsclients_mount.sh
-echo "echo \"`hostname`:/home   /home   nfs    auto  0  0\"" >> nfsclients_mount.sh
-echo "echo \"`hostname`:/var/dev   /dev   nfs    auto  0  0\"" >> nfsclients_mount.sh
-echo "echo \"`hostname`:/var/config   /config   nfs    auto  0  0\"" >> nfsclients_mount.sh
+echo "echo \"#####################    Adding /var/nfs mount points to /etc/fstab #####################\"" >> nfsclients_mount.sh
+#echo "echo \"#####################    Echo into fstab on CLIENT   #####################\"" >> nfsclients_mount.sh
+echo "echo \"`hostname`:/home   /var/nfs/home   nfs    auto  0  0\" >> /etc/fstab" >> nfsclients_mount.sh
+echo "echo \"`hostname`:/var/dev   /var/nfs/dev   nfs    auto  0  0\" >> /etc/fstab" >> nfsclients_mount.sh
+echo "echo \"`hostname`:/var/config   /var/nfs/config   nfs    auto  0  0\" >> /etc/fstab" >> nfsclients_mount.sh
 echo "Copy the file nfsclients_mount.sh to the client and run it there."
