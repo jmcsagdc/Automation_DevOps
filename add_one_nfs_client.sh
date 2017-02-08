5dd51426b82a319e8fa6121c9412a752d91531b1
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-07 16:25:08 -0800

    Update add_one_nfs_client.sh
    again, whitespace

--- add_one_nfs_client.sh
+++ add_one_nfs_client.sh
@@ -2,11 +2,11 @@
 # This takes one IP in 'single quotes' as an argument
 
 IP=$1
-echo "/home		$IP(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/home             $IP(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 echo "echo \"/home   $IP(rw,sync,no_root_squash,no_subtree_check)\" >> /etc/exports"
-echo "/dev		$IP(rw,sync,no_subtree_check)"                >> /etc/exports
+echo "/dev              $IP(rw,sync,no_subtree_check)"                >> /etc/exports
 echo "echo \"/dev    $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
-echo "/config	$IP(rw,sync,no_subtree_check)"                >> /etc/exports
+echo "/config           $IP(rw,sync,no_subtree_check)"                >> /etc/exports
 echo "echo \"/config $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
 
 exportfs -a
