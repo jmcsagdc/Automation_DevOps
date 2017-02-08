71525c95f4a48eb2a4be42330b0350fc4e1ee3df
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-07 16:20:31 -0800

    Update add_one_nfs_client.sh
    whitespace

--- add_one_nfs_client.sh
+++ add_one_nfs_client.sh
@@ -2,12 +2,12 @@
 # This takes one IP in 'single quotes' as an argument
 
 IP=$1
-echo "/home		$IP(rw,sync,no_root_squash,no_subtree_check)"	>> /etc/exports
-echo "echo \"/home		$IP(rw,sync,no_root_squash,no_subtree_check)\"	>> /etc/exports"
-echo "/dev		$IP(rw,sync,no_subtree_check)"					>> /etc/exports
-echo "echo \"/dev		$IP(rw,sync,no_subtree_check)\"					>> /etc/exports"
-echo "/config	$IP(rw,sync,no_subtree_check)"					>> /etc/exports
-echo "echo \"/config	$IP(rw,sync,no_subtree_check)\"					>> /etc/exports"
+echo "/home		$IP(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "echo \"/home   $IP(rw,sync,no_root_squash,no_subtree_check)\" >> /etc/exports"
+echo "/dev		$IP(rw,sync,no_subtree_check)"                >> /etc/exports
+echo "echo \"/dev    $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
+echo "/config	$IP(rw,sync,no_subtree_check)"                >> /etc/exports
+echo "echo \"/config $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
 
 exportfs -a
 echo "exportfs -a"
