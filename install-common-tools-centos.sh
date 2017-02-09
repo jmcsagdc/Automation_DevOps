211bd70a406f3edadce2501c1dfcbae988457687
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-09 12:49:17 -0800

    Update install-common-tools-centos.sh
    adds check for os

--- install-common-tools-centos.sh
+++ install-common-tools-centos.sh
@@ -1,3 +1,18 @@
+# Run this on NFS SERVER and scp the output file to the clients for now
+myKernel=$(uname -r | grep 'generic')
+
+echo 'myKernel is ' $myKernel
+
+if uname -r | grep 'generic' 1>/dev/null
+then
+  echo "Wrong OS" # If Ubuntu is NOT target OS
+  exit 1
+else
+  echo "CentOS is correct for server"
+  #echo "Wrong OS" # If redhat is NOT target
+  #exit 1
+fi
+
 echo "Installing nano text editor..."
 yum install -y nano
 
