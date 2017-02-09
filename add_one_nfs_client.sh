e32c680eaf5893a8d45667f75ed724cbdc207082
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-09 10:03:06 -0800

    Update add_one_nfs_client.sh
    fixed bad config and dev locations to /var/...

--- add_one_nfs_client.sh
+++ add_one_nfs_client.sh
@@ -4,10 +4,10 @@
 IP=$1
 echo "/home             $IP(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 echo "echo \"/home   $IP(rw,sync,no_root_squash,no_subtree_check)\" >> /etc/exports"
-echo "/dev              $IP(rw,sync,no_subtree_check)"                >> /etc/exports
-echo "echo \"/dev    $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
-echo "/config           $IP(rw,sync,no_subtree_check)"                >> /etc/exports
-echo "echo \"/config $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
+echo "/var/dev              $IP(rw,sync,no_subtree_check)"                >> /etc/exports
+echo "echo \"/var/dev    $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
+echo "/var/config           $IP(rw,sync,no_subtree_check)"                >> /etc/exports
+echo "echo \"/var/config $IP(rw,sync,no_subtree_check)\"                >> /etc/exports"
 
 exportfs -a
 echo "exportfs -a"
