54566a238122d023ddc63ecfab93c58d59bb0d8f
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-17 10:21:53 -0800

    Update utility-adjust-harden-centos.sh

--- utility-adjust-harden-centos.sh
+++ utility-adjust-harden-centos.sh
@@ -3,7 +3,7 @@ echo "Back up sudoers"
 cp /etc/sudoers /etc/sudoers.orig
 echo "Only adm members get sudo..."
 perl -pi -e 's|## Allows people in group wheel to run all commands|## Allows people in group adm to run all commands|g' /etc/sudoers
-perl -pi -e 's|%wheel    ALL=(ALL)       ALL|%adm    ALL=(ALL)       ALL|g' /etc/sudoers
+perl -pi -e 's|\x25wheel|\x25adm|g' /etc/sudoers
 
 # Disallow SSH for regions outside US
 
