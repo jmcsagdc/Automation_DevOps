c0206386f3c21f06764e1d6c7b65d64d3b647bca
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-25 11:53:14 -0800

    Update install-common-tools-generic.sh

--- install-common-tools-generic.sh
+++ install-common-tools-generic.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 sudo su
-myKernel=$(uname -r | grep 'generic')
+git clone https://github.com/jmcsagdc/Automation_NTI-310.git /root/Automation
 
-echo 'myKernel is ' $myKernel
+myKernel=$(uname -r | grep 'generic')
+echo "myKernel is $myKernel"  >> /root/INSTALL.LOG
 
 if uname -r | grep 'generic' 1>/dev/null
 then
+  # Ubuntu Desktop
+  chmod +x /root/Automation/*
+  chmod -x /root/Automation/*server*.sh
+  chmod -x /root/Automation/*centos*.sh
+  chmod -x /root/Automation/*.py
+  #./install-common-tools-ubuntu.sh >> /root/INSTALL.LOG 2>&1 # Use this
   echo "Ubuntu OS is correct for Desktops" >> /root/INSTALL.LOG
   echo "Installing tree utility" >> /root/INSTALL.LOG
   apt install -y tree
@@ -13,13 +20,9 @@ then
   apt install -y htop
   echo "git pull jv's automation tools" >> /root/INSTALL.LOG
   echo "cloning repo to /root/Automation"  >> /root/INSTALL.LOG
-  git clone https://github.com/jmcsagdc/Automation_NTI-310.git /root/Automation
-  chmod +x /root/Automation/*ubuntu*.sh
-  chmod -x /root/Automation/*server*.sh
-  chmod -x /root/Automation/*centos*.sh
-  chmod +x /root/Automation/*all*.sh
 else
-  echo "CentOS is correct for server"
+  # CentOS Server
+  echo "CentOS is correct for server"  >> /root/INSTALL.LOG
   echo "Installing networking tools..." >> /root/INSTALL.LOG
   yum install -y bind-utils
   echo "Installing wget..." >> /root/INSTALL.LOG
@@ -34,9 +37,11 @@ else
   yum install -y mlocate
   echo "git pull jv's automation tools" >> /root/INSTALL.LOG
   echo "cloning repo to /root/Automation"  >> /root/INSTALL.LOG
-  git clone https://github.com/jmcsagdc/Automation_NTI-310.git /root/Automation
-  chmod +x /root/Automation/*server*.sh
-  chmod +x /root/Automation/*centos*.sh
-  chmod +x /root/Automation/*all*.sh
+  chmod +x /root/Automation/*
+  chmod -x /root/Automation/*ubuntu*.sh
   chmod -x /root/Automation/*.py
 fi
+#TODO Add log lines for everything
+#TODO Fix the chmods so they actually work
+#TODO Modularize this. The ifs should use your scripts, not replace them.
+#TODO Move git clone before the if
