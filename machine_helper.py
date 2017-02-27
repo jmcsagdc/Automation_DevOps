1e032f725b7205dae0ef3b79dd225507c13c41f8
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-02-27 15:38:37 -0800

    Update machine_helper.py

--- machine_helper.py
+++ machine_helper.py
@@ -11,6 +11,11 @@ for i in range(0, len(getMyType_l)):
 print myInstall
 if '1' in myInstall:
     print 'I is Desktop'
+    # Client machine
+    doInstall=os.popen('./utility-adjust-ssh-all-os.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('./install-ldap-client-ubuntu.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('touch /root/use-NFS-client-script-creator-on-server')
+  # Figure out NFS client builder from CentOS server.
 if '2' in myInstall:
     print 'I is LDAP'
 if '3' in myInstall:
