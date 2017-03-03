b12c57a03bf0412383057436cba99a1efcddbc44
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-03-02 18:49:41 -0800

    Update install-ldap-server-centos.sh

--- install-ldap-server-centos.sh
+++ install-ldap-server-centos.sh
@@ -274,3 +274,6 @@ firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --reload
 
 echo "ldap configuration complete. Point your browser to http://<serverIPaddress>/phpldapadmin to login..."
+echo "************************** HARDENING **************************" >> /root/INSTALL.LOG
+echo "Running /root/Automation/utility-adjust-harden-centos.sh"  >> /root/INSTALL.LOG
+/root/Automation/utility-adjust-harden-centos.sh >> /root/INSTALL.LOG
