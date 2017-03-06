2d7a26aece3a5570028578c328384626d548f3f2
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-03-06 14:46:51 -0800

    Create utility-update-ldap-client-installer.sh

---
+++ utility-update-ldap-client-installer.sh
@@ -0,0 +1,20 @@
+import os
+
+hostnameBase='server-ldap-' # Build base of ldap server hostname
+
+myHostname=os.popen('hostname').read()
+mySubnet=''
+count=0
+mySubnet_l=myHostname.strip().split('-')
+
+for i in range(1, len(mySubnet_l)):
+    if count==0:
+        mySubnet+=mySubnet_l[i]
+    else:
+        mySubnet+='-'+mySubnet_l[i]
+    count+=1
+myLdapServer=hostnameBase+mySubnet
+print myLdapServer
+
+# All this does is construct a hostname for 
+# substitution into LDAP Server Create Script
