10fda25579fb642ef9bab4a9f30003990373af32
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-03-11 10:41:55 -0800

    Create install-syslog-client-ubuntu.py

---
+++ install-syslog-client-ubuntu.py
@@ -0,0 +1,29 @@
+import os
+
+hostnameBase='server-rsyslog-' # Build base of rsyslog server hostname
+
+myHostname=os.popen('hostname').read()
+mySubnet=''
+count=0
+mySubnet_l=myHostname.strip().split('-')
+
+for i in range(1, len(mySubnet_l)):
+    if count==0:
+        mySubnet+=mySubnet_l[i]
+    else:
+        mySubnet+='-'+mySubnet_l[i]
+    count+=1
+myRsyslogServer=hostnameBase+mySubnet
+print myRsyslogServer
+
+outfile=open('rsyslogclients_mount.sh','w')
+outfile.write('echo "*.info;mail.none;authpriv.none;cron.none    @'+myRsyslogServer+'" >> /etc/rsyslog.conf')
+outfile.write('service rsyslog restart')
+
+# Big sleep avoids race condition where client tries to mount a server not up yet.
+
+pyRun=os.popen('sleep 60').read()
+pyRun=os.popen('echo "************* Changing perms /root/Automation/rsyslogclients_mount.sh ************" >> /root/INSTALL.LOG 2>&1').read()
+pyRun=os.popen('chmod +x /root/Automation/rsyslogclients_mount.sh').read()
+pyRun=os.popen('echo "************* Running /root/Automation/rsyslogclients_mount.sh ************" >> /root/INSTALL.LOG 2>&1').read()
+pyRun=os.popen('/root/Automation/rsyslogclients_mount.sh').read()
