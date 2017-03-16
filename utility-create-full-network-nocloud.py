c440d1d92894dbcf2c7db00efa90e5111feefd9f
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-03-16 10:30:46 -0700

    use this interactively to deploy a full network

--- utility-create-full-network-nocloud.py
+++ utility-create-full-network-nocloud.py
@@ -16,13 +16,13 @@ import os
 desktops=[]
 servers=[]
 extras=[]
+myServers_l=[]
 
 # Full startup-script creator
 
 outfile=open('advanced-complete-install.sh','w')
 newScriptFile='''#!/bin/bash
 sudo su
-
 if uname -r | grep 'generic' 1>/dev/null
 then
   true # Ubuntu has git. Do nothing.
@@ -30,18 +30,14 @@ else
   yum install -y git
   # Because the light install of CentOS doesn't have git.
 fi
-
 # Pull everything for next step
 echo "git clone jmcs automation tools to /root/Automation" >> /root/INSTALL.LOG
 git clone https://github.com/jmcsagdc/Automation_NTI-310.git \\
   /root/Automation  >> /root/INSTALL.LOG 2>&1
-
 myKernel=$(uname -r | grep 'generic')
 echo "myKernel is $myKernel"  >> /root/INSTALL.LOG
-
 cd /root/Automation
 chmod +x *
-
 if uname -r | grep 'generic' 1>/dev/null
 then
   # Ubuntu Desktop
@@ -53,10 +49,8 @@ else
   chmod -x *ubuntu*.sh
   ./install-common-tools-centos.sh >> /root/INSTALL.LOG 2>&1
 fi
-
 chmod -x *.py
 chmod -x *.md
-
 python machine_helper.py
 '''
 outfile.write(newScriptFile)
@@ -85,21 +79,18 @@ def buildGcloudMachine(createMachineName,imageType,projectName,machineinstalltyp
 
 def getServerList():
     myServers=os.popen('gcloud compute instances list --uri | awk -F/ \'{print $11}\'').read()
-    #print myServers #DEBUG
-    myServers=myServers.split('\n')
-
-    for each in myServers:
+    myServers_l=myServers.split('\n')
+    
+    for each in myServers_l:
         if "desktop" in each:
             desktops.append(each)
         elif "server" in each:
             servers.append(each)
         else:
             extras.append(each)
-    return servers
-    return desktops
-    return extras
+    return myServers
 
-getServerList()
+myServers=getServerList()
 print '*****************************************'
 print '       Currently deployed servers'
 print '*****************************************\n'
@@ -113,9 +104,19 @@ print '\n   Extras: '
 for each in extras:
     print each
 
+moveOn=False
+while moveOn==False:
+    askUser='Which network group name to create? '
+    createNetworkName=raw_input(askUser)
+    if createNetworkName in myServers:
+        print 'Try again.'
+    else:
+        moveOn=True
 
-askUser='Which network group name to create?'
-createNetworkName=raw_input(askUser)
+askUser='How many Desktops for users to create? '
+createDesktopsQuantity=raw_input(askUser)
+if createDesktopsQuantity=='':
+    createDesktopsQuantity=1
 
 # Moved desktop to last. It mounts locations that must exist.
 #TODO The order here is not numeric. Simply convenient. Find a better way.
@@ -165,11 +166,12 @@ machineinstalltype='5'
 myTags='--tags http-server,https-server'
 buildGcloudMachine(createMachineName,imageType,projectName,machineinstalltype,myTags)
 
-#desktop
-createMachineName=''
-createMachineName='desktop-'+createNetworkName
-imageType='ubuntu-1604-lts'
-projectName='ubuntu-os-cloud'
-machineinstalltype='1'
-myTags=''
-buildGcloudMachine(createMachineName,imageType,projectName,machineinstalltype,myTags)
+for i in range(1, int(createDesktopsQuantity)+1):
+    #desktop
+    createMachineName=''
+    createMachineName='desktop-'+createNetworkName+'-'+str(i)
+    imageType='ubuntu-1604-lts'
+    projectName='ubuntu-os-cloud'
+    machineinstalltype='1'
+    myTags=''
+    buildGcloudMachine(createMachineName,imageType,projectName,machineinstalltype,myTags)
