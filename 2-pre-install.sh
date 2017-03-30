6df687c3c500aa19630f5ac445a56c431111b177
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-03-30 07:38:36 -0700

    Create 2-pre-install.sh

---
+++ 2-pre-install.sh
@@ -0,0 +1,3 @@
+echo "This file is not actually used in this process."
+echo "The python create script generates the real script of this name"
+echo "outside of the cloud and passes that script in to each server via metadata."
