e87cf6be95037850d5a19791d6d7a0a62db9bdef
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-04 14:45:48 -0700

    Update install-nagios-client-centos.sh

--- install-nagios-client-centos.sh
+++ install-nagios-client-centos.sh
@@ -9,9 +9,8 @@ NAGIOSSERVER=$(python helper-get-nagios.py)
 echo "Find and replace in the allowed_hosts with private IP of Nagios server"
 
 perl -pi -e "s|allowed_hosts=127.0.0.1|allowed_hosts=127.0.0.1,$NAGIOSSERVER|g" /etc/nagios/nrpe.cfg
-Save and exit. This configures NRPE to accept requests from your Nagios server, via its private IP address.
 
-Restart NRPE to put the change into effect:
+echo "restart NRPE to pick up change"
 
 sudo systemctl start nrpe.service
 sudo systemctl enable nrpe.service
