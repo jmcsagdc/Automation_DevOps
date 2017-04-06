9cbdea0c30c6dd66d9fb4686b4bd6eb4ba1456f0
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-06 14:27:00 -0700

    Update utility-add-nagios-client-configs.py

--- utility-add-nagios-client-configs.py
+++ utility-add-nagios-client-configs.py
@@ -1,23 +1,25 @@
+
 from helpers import *
+import os
+
+pyRun=os.popen('mkdir tmpFiles').read()
+print "Created tmpFiles dir. If script fails, check here for configs."
 
 myServers, names, local_ips=GetCloudHostsData()
-#print myServers
 
-#myRows=myServers.split('\n')
-#print myRows
+myNetwork=GetMyNetworkName()
+print myNetwork
 
-#name=['bobross','freddkrueger']
-#ip=['1.2.3.4','5.6.7.8']
-
+myFile=''
 
 for i in range(0, len(names)):
 
-    print '''define host {
+    myFile='''define host {
         use                             linux-server
         host_name                       '''+names[i]+'\n'
-    print '''        alias                           My first postgres server
+    myFile+='''        alias                           My first postgres server
         address                         '''+local_ips[i]+'\n'
-    print '''        max_check_attempts              5
+    myFile+= '''        max_check_attempts              5
         check_period                    24x7
         notification_interval           30
         notification_period             24x7
@@ -26,10 +28,17 @@ for i in range(0, len(names)):
 define service {
         use                             generic-service
         host_name                       '''+names[i]+'\n'
-    print '''        service_description             PING
+    myFile+= '''        service_description             PING
         check_command                   check_ping!100.0,20%!500.0,60%
 }'''
 
+    outfileName='tmpFiles/'+names[i]+'.cfg'
+    outfile=open(outfileName,'w')
+    outfile.write(myFile)
+    outfile.close()
+
+myPath='tmpFiles/*'+myNetwork+'*'
+myCommand='cp '+myPath+' /usr/local/nagios/etc/servers/.'
+pyRun=os.popen(myCommand).read()
+print pyRun
 print 'END'
-print names
-print local_ips
