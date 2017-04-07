cb50ff81c8ff4e84df7866dfd6e7a360cf5955ef
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-06 19:57:36 -0700

    Update install-nagios-client-centos.sh

--- install-nagios-client-centos.sh
+++ install-nagios-client-centos.sh
@@ -14,8 +14,33 @@ echo "Find and replace in the allowed_hosts with private IP of Nagios server"
 
 perl -pi -e "s|allowed_hosts=127.0.0.1|allowed_hosts=127.0.0.1,$NAGIOSIP|g" /etc/nagios/nrpe.cfg
 
-sudo systemctl start nrpe.service
-sudo systemctl enable nrpe.service
+echo "selinux: setenforce 0 before starting the nrpe service
+setenforce 0
 
-#TODO: Need server side addition below after we do this...
-#/usr/local/nagios/etc/servers/server-sql1-train.cfg this server-side config needs to be automated
+sudo systemctl enable nrpe.service
+sudo systemctl start nrpe.service
+
+# Create this file in /etc/nrpe.d/op5_commands.cfg
+################################################################################
+#
+# op5-nrpe command configuration file
+#
+# COMMAND DEFINITIONS
+# Syntax:
+#       command[<command_name>]=<command_line>
+#
+echo "command[users]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10
+command[load]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20
+command[check_load]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20
+command[swap]=/usr/lib64/nagios/plugins/check_swap -w 20% -c 10%
+command[root_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p / -m
+command[usr_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /usr -m
+command[var_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /var -m
+command[zombie_procs]=/usr/lib64/nagios/plugins/check_procs -w 5 -c 10 -s Z
+command[total_procs]=/usr/lib64/nagios/plugins/check_procs -w 190 -c 200
+command[proc_named]=/usr/lib64/nagios/plugins/check_procs -w 1: -c 1:2 -C named
+command[proc_crond]=/usr/lib64/nagios/plugins/check_procs -w 1: -c 1:5 -C crond
+command[proc_syslogd]=/usr/lib64/nagios/plugins/check_procs -w 1: -c 1:2 -C syslog-ng
+command[proc_rsyslogd]=/usr/lib64/nagios/plugins/check_procs -w 1: -c 1:2 -C rsyslogd" >> /etc/nrpe.d/op5_commands.cfg
+
+echo "DONE!"
