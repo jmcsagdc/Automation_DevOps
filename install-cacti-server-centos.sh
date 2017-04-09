6ad783334ac4ffb8e0fd1a2abe6b944daf6f4662
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-09 10:11:40 -0700

    Update install-cacti-server-centos.sh

--- install-cacti-server-centos.sh
+++ install-cacti-server-centos.sh
@@ -44,3 +44,20 @@ echo "perform config file user substitutions"
 perl -pi -e "s|database_username = 'cactiuser'|database_username = 'root'|g" /etc/cacti/db.php
 perl -pi -e "s|database_password = 'cactiuser'|database_password = 'Passw0rd'|g" /etc/cacti/db.php
 #TODO maybe change this to a different user
+
+echo "Back up /etc/httpd/conf.d/cacti.conf as orig"
+cp /etc/httpd/conf.d/cacti.conf /etc/httpd/conf.d/cacti.conf.orig
+
+echo "Modify webserver permissions"
+perl -pi -e "s|Require host localhost|Require all granted|g" /etc/httpd/conf.d/cacti.conf
+perl -pi -e "s|Allow from localhost|Allow from all|g" /etc/httpd/conf.d/cacti.conf
+
+echo "At this point you can verify that something is visible in <IP>/cacti/install"
+
+echo "Back up /etc/cron.d/cacti as orig"
+cp /etc/cron.d/cacti /etc/cron.d/cacti.orig
+
+echo "Overwrite /etc/cron.d/cacti with desired polling time"
+echo '*/5 * * * *    cacti   /usr/bin/php /usr/share/cacti/poller.php > /dev/null 2>&1' > /etc/cron.d/cacti
+
+echo "MANUAL INSTALLING VIA WEBPAGES"
