c7d123304585955ce769956c6c7083d917fc1c73
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-09 12:31:02 -0700

    Update install-cacti-server-centos.sh

--- install-cacti-server-centos.sh
+++ install-cacti-server-centos.sh
@@ -87,6 +87,9 @@ echo "Back up /etc/snmp/snmpd.conf as orig"
 cp /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.orig
 
 echo "Modify snmpd.conf"
+NETWORK=$(./get_vm_ip.sh `hostname` | awk -F '.' '{ print $1"."$2"."$3".0/24" }')
+
+perl -pi -e "s|com2sec notConfigUser  default       public|com2sec notConfigUser  $NETWORK     public|g" testfile.config
 perl -pi -e "s|notConfigGroup|myGroup|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|notConfigUser|myUser|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|group have rights to:|group have rights to:\nview\tall\tincluded\t.1|g" /etc/snmp/snmpd.conf
@@ -95,3 +98,8 @@ perl -pi -e "s|exact  systemview none none|exact  all all none|g" /etc/snmp/snmp
 echo "Enable and restart snmpd"
 systemctl enable snmpd.service
 systemctl restart snmpd.service
+
+echo "Verify by running snmpwalk"
+echo "snmpwalk -c myCommunity `/root/Automation/get_vm_ip.sh $HOSTNAME` -v1"
+
+echo "Moving on..."
