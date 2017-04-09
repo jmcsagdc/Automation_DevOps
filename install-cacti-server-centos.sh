15c15df5e90c7144678671427f98e733a25e5155
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-09 09:23:25 -0700

    Update install-cacti-server-centos.sh

--- install-cacti-server-centos.sh
+++ install-cacti-server-centos.sh
@@ -1,4 +1,4 @@
-echo "Install cacti"
+echo "Install cacti dependencies"
 
 yum install -y httpd httpd-devel
 yum install -y mysql mysql-server
@@ -14,5 +14,17 @@ systemctl start httpd.service
 systemctl start snmpd.service
 systemctl start mariadb.service
 
-echo "Moving on..."
+echo "Enable these to start on boot"
 
+systemctl enable httpd.service
+systemctl enable mariadb.service
+systemctl enable snmpd.service
+
+echo "Install cacti"
+
+yum install -y cacti
+
+echo "Add mysql root user password ( see /root/mysql_root_pass )"
+mysqladmin -u root password Passw0rd
+echo "Passw0rd" >> /root/mysql_root_pass
+chmod 600 /root/mysql_root_pass
