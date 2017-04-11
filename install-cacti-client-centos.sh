a570c2105ce302c2916d8fa74478c037b957dafe
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-11 09:21:58 -0700

    Update install-cacti-client-centos.sh

--- install-cacti-client-centos.sh
+++ install-cacti-client-centos.sh
@@ -17,10 +17,10 @@ cp /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.orig
 echo "Modify snmpd.conf"
 NETWORK=$(./get_vm_ip.sh `hostname` | awk -F '.' '{ print $1"."$2"."$3".0/24" }')
 
-perl -pi -e "s|com2sec notConfigUser  default       public|com2sec notConfigUser  $NETWORK     public|g" /etc/snmp/snmpd.conf
+perl -pi -e "s|com2sec notConfigUser  default       public|com2sec myUser  $NETWORK     myCommunity|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|notConfigGroup|myGroup|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|notConfigUser|myUser|g" /etc/snmp/snmpd.conf
-perl -pi -e "s|group have rights to:|group have rights to:\nview\tall\tincluded\t.1|g" /etc/snmp$
+perl -pi -e "s|group have rights to:|group have rights to:\nview\tall\tincluded\t.1|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|exact  systemview none none|exact  all all none|g" /etc/snmp/snmpd.conf
 
 echo "Enable and restart snmpd"
