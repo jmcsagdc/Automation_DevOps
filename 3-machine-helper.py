d246011405a98779e3cc09a2d9262c48cdf1dc3a
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-04-30 11:58:58 -0700

    Update 3-machine-helper.py

--- 3-machine-helper.py
+++ 3-machine-helper.py
@@ -14,6 +14,7 @@ if 'ubuntudesktop' in getMyType:
     doInstall=os.popen('echo "****** USED Python NFS client installer ****************" >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-rsyslog-client-50-default-all-os.py')
     doInstall=os.popen('echo "****** USED Python rsyslog client installer ************" >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7ldap' in getMyType:
     print 'LDAP'
     # LDAP Server
@@ -25,6 +26,7 @@ if 'centos7ldap' in getMyType:
     doInstall=os.popen('./utility-adjust-harden-centos.sh >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-syslog-client-all-os.py')
     doInstall=os.popen('./install-nagios-client-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7nfs' in getMyType:
     print 'NFS'
     # NFS Server
@@ -36,6 +38,7 @@ if 'centos7nfs' in getMyType:
     doInstall=os.popen('./utility-adjust-harden-centos.sh >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-syslog-client-all-os.py')
     doInstall=os.popen('./install-nagios-client-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7sql' in getMyType:
     print 'Postgres'
     # Postgres Server
@@ -44,6 +47,7 @@ if 'centos7sql' in getMyType:
     doInstall=os.popen('./utility-adjust-harden-centos.sh >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-syslog-client-all-os.py')
     doInstall=os.popen('./install-nagios-client-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7django' in getMyType:
     print 'Django'
     # Django and Apache server
@@ -54,6 +58,7 @@ if 'centos7django' in getMyType:
     doInstall=os.popen('echo "****** CHANGE FIREWALL IN CLOUD FOR PORT 8000 ****************" >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-syslog-client-all-os.py')
     doInstall=os.popen('./install-nagios-client-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7plain' in getMyType:
     print 'PLAIN'
     # Plain Server
@@ -61,6 +66,7 @@ if 'centos7plain' in getMyType:
     doInstall=os.popen('./utility-adjust-harden-centos.sh >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-syslog-client-all-os.py')
     doInstall=os.popen('./install-nagios-client-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7syslog' in getMyType:
     print 'rsyslog'
     # rsyslog Server
@@ -68,6 +74,7 @@ if 'centos7syslog' in getMyType:
     doInstall=os.popen('./install-syslog-server-centos.sh >> /root/INSTALL.LOG 2>&1')
     # DO DOT add the rsyslog client install here.
     doInstall=os.popen('./install-nagios-client-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 if 'centos7nagios' in getMyType:
     print 'Nagios'
     # Nagios server
@@ -78,4 +85,13 @@ if 'centos7nagios' in getMyType:
     doInstall=os.popen('echo "****** CHANGE FIREWALL IN CLOUD FOR PORT 5666 ****************" >> /root/INSTALL.LOG 2>&1')
     doInstall=os.popen('python install-syslog-client-all-os.py')
     doInstall=os.popen('python utility-add-nagios-client-configs.py')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
+if 'centos7build' in getMyType:
+    print 'Build'
+    # Build server
+    doInstall=os.popen('echo "*** Build *** ">> /root/INSTALL.LOG')
+    doInstall=os.popen('/root/Automation/install-build-server-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('./utility-adjust-harden-centos.sh >> /root/INSTALL.LOG 2>&1')
+    doInstall=os.popen('python install-syslog-client-all-os.py')
+    doInstall=os.popen('echo "*** End Helper *** ">> /root/INSTALL.LOG')
 print 'END!!!'
