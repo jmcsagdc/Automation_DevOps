a04ea8a54377c89b7bbad55ada7ee9abea603122
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-06-10 19:20:12 -0700

    Update install-cacti-server-centos.sh

--- install-cacti-server-centos.sh
+++ install-cacti-server-centos.sh
@@ -103,7 +103,7 @@ cp /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.orig
 echo "Modify snmpd.conf"
 NETWORK=$(./get_vm_ip.sh `hostname` | awk -F '.' '{ print $1"."$2"."$3".0/24" }')
 
-perl -pi -e "s|com2sec notConfigUser  default       public|com2sec myUser  $NETWORK     myCommunity|g" testfile.config
+perl -pi -e "s|com2sec notConfigUser  default       public|com2sec myUser  $NETWORK     myCommunity|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|notConfigGroup|myGroup|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|notConfigUser|myUser|g" /etc/snmp/snmpd.conf
 perl -pi -e "s|group have rights to:|group have rights to:\nview\tall\tincluded\t.1|g" /etc/snmp/snmpd.conf
