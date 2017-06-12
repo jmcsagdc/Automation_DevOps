1466c90799d8553fa3be3c2ce4b0c44ccd7d1cec
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-06-12 14:38:13 -0700

    added gcloudCopier

--- helpers.py
+++ helpers.py
@@ -1,3 +1,41 @@
+def gcloudCopier(myDestination='',mySourceFile=''):
+    import os
+    import sys
+
+    if myDestination!='' and mySourceFile!='':   # If there are args
+        copyToServer=myDestination # Set copyToServer to that arg
+        #print "DEBUG: "+myDestination+"   DEBUG"
+        sourcePath=mySourceFile # Set sourcePath to that arg
+    else:
+        myHosts_l=[]
+        #print 'DEBUG list displays. Ignore it.'
+        results_l=GetCloudHostsData()
+        servers_dict=results_l[0]
+        for k,v in servers_dict.iteritems():
+            #print k,v #DEBUG
+            myHosts_l.append(k)
+        #print '\n\n'
+        #print 'Here are the current servers available:'
+        #for each in myHosts_l:
+        #    print each
+
+        messageString='Please enter the name of the host to copy to:  '
+        copyToServer=raw_input(messageString)
+        messageString='Please enter path of file and filename to copy:  '
+        sourcePath=raw_input(messageString)
+        if copyToServer not in myHosts_l:
+            print "Don't be silly"
+            exit()
+        else:
+            pass
+    destPath='~/.'
+    copyString='gcloud compute copy-files --zone us-central1-c '
+    copyString+=sourcePath+' '+copyToServer+':'+destPath
+    print copyString #DEBUG
+    print 'EXECUTING'
+    pyRun=os.popen(copyString).read()
+    print pyRun
+
 def gcloudCommander(argCommand=''):
     import os
     import sys
