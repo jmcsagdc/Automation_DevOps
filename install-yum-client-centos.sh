36e0f5fdbc06aeee5fdabf863401e329a9aa7850
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-06-15 12:08:39 -0700

    Create install-yum-client-centos.sh

---
+++ install-yum-client-centos.sh
@@ -0,0 +1,20 @@
+echo "Installing CLIENT YUM REPOSITORY"
+
+myYumServer=$('gcloud compute instances list | grep "yum\|repo" | awk "{ print $1 }"') 
+
+#nano /etc/yum.repos.d/myrepo.repo
+echo "[myrepo]
+name=Network Repository
+baseurl=ftp://$myYumServer/pub/localrepo/CentOS/7/0/x86_64
+gpcheck=0
+enabled=1" > /etc/yum.repos.d/myrepo.repo
+
+yum update
+
+yum repolist
+
+yum --disablerepo "*" --enablerepo "myrepo" list available
+
+setenforce 0
+
+echo "Finished installing CLIENT YUM REPOSITORY"
