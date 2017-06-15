d72b5c9fb84acce498f6ca006c1bfeaa2613778a
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-06-14 21:19:59 -0700

    Create check_process.sh

--- check_process.sh
+++ check_process.sh
@@ -26,35 +26,36 @@
 # Step three: Check myState and act appropriately
 # just check for each state and exit. if neither alert nor warn, assume good.
 
-check_process() {
+#check_process() {
 myState==0
 aProcessNameList=`ps -ax | awk '{ print $5 }' | sort | uniq`
 aProcessStateList=`ps -ax | awk '{ print $3 }'`
-#echo $aProcessStateList
-#echo $aProcessNameList
 
-# Step one: Check for missing processes
-for i in $*; do   # This iterates through the list of process names from command line args
-  if echo $aProcessNameList | grep -q $i; then   # is this arg 'i' found in the contents of aProcessNameList?
-    echo "`date` [healthcheck] - Found $i >> /var/log/messages"
+#for i in $*; do echo "$i"; done
+                                                           # Step one: Check for missing processes
+for i in $*; do                                            # Iterate list of process names from args
+  if echo $aProcessNameList | grep -q $(echo "$i"); then   # is arg 'i' found in aProcessNameList?
+    myState=0                                              # DEBUG we are OK
   else
-    echo "`date` [healthcheck] - $i MISSING" >> /var/log/messages; myState=2    # I'm missing an expected process. Set myState to ALERT
+    myState=2                                              # I'm missing an expected process. Set myState to ALERT
   fi
 done
 
 # Step two: Check for zombies
 if echo $aProcessStateList | grep -q "Z"; then  # is the Z for zombie found in the contents of aProcessStateList?
-  echo "`date` [healthcheck] - Zombie Found" >> /var/log/messages; myState=1  # I found a Zombie. Set myState to WARN
+  myState=1  # I found a Zombie. Set myState to WARN
 fi
 
 # Step three: Check myState and act appropriately
-if [[ $myState==2 ]]; then
+if [[ $myState -eq 2 ]]; then
+  echo "CRITICAL"
   exit 2
 fi
 
-if [[ $myState==1 ]]; then
+if [[ $myState -eq 1 ]]; then
+  echo "WARN"
   exit 1
 fi
 
-echo "`date` [healthcheck] - I'm GOOD" >> /var/log/messages; exit 0
-}
+echo "GOOD"; exit 0
+#}
